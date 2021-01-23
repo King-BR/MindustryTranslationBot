b7251595340d5f3fe62b313e5cf5772d7d87579f
Fernando <fernandopach2017@gmail.com>
2021-01-23 23:27:45 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,152 @@
+// Bot online 24/7
+require("./keep_alive.js");
+
+// NPMs
+const Discord = require("discord.js");
+const fs = require("fs");
+const Enmap = require("enmap");
+
+// Bot config
+require("dotenv").config();
+const config = require("./config.json");
+const botUtils = require("./utils.js");
+const client = new Discord.Client({
+  fetchAllMembers: true,
+  disableMentions: "everyone",
+  autoreconnect: true,
+  partials: ['MESSAGE', 'REACTION']
+});
+client.config = config;
+
+// Utils config
+chalkClient = botUtils.chalkClient;
+newError = botUtils.newError;
+isDir = botUtils.isDir;
+botUtils.clearAllErrors();
+
+// Handler de erros em promise
+process.on('unhandledRejection', (reason, p) => { console.log(reason) });
+
+// Starting Time - by Alstin112
+botUtils.jsonChange('./dataBank/serverState.json', server => {
+  server["serverStarted"] = (new Date()).getTime() - 10800000
+  return server;
+},true);
+
+// Event handler
+console.log('\n------------------\nEvents');
+let source = fs.readdirSync("./events");
+source.forEach(foldert => {
+  var sla = fs.readdirSync(`./events/${foldert}`);
+  console.log(`\n${foldert}/`);
+  sla.forEach(filet => {
+    try {
+      if (!filet.endsWith(".js")) {
+        if (isDir(`./events/${foldert}/${filet}`) && filet == "utils") {
+          var utilst = fs.readdirSync(`./events/${foldert}/${filet}`);
+          console.log(`- ${filet}/`);
+          utilst.forEach(fileutils => {
+            let nameutil = foldert + fileutils.split(".")[0];
+            try {
+              require(`./events/${foldert}/${filet}/${fileutils}`);
+              console.log(`- - ${fileutils}: ${chalkClient.ok('OK')}`)
+            } catch (err) {
+              console.log(`- - ${fileutils}: ${chalkClient.error('ERROR')}`);
+              console.log(`=> ${newError(err, nameutil)}`);
+            }
+          })
+        }
+        return;
+      }
+      let name = filet.split('.')[0];
+      console.log(`- ${name}.js: ${chalkClient.ok('OK')}`);
+      let exported = require(`./events/${foldert}/${filet}`);
+      client.on(name, exported.bind(null, client));
+    } catch (err) {
+      console.log(`- ${filet}: ${chalkClient.error('ERROR')}`);
+      console.log(`=> ${newError(err, filet)}`);
+    }
+  });
+});
+
+// Command handler setup
+client.commands = new Discord.Collection();
+client.commands = new Enmap();
+client.aliases = new Discord.Collection();
+let commandsFolder = fs.readdirSync("commands");
+
+// Config Utils handler setup
+client.utils = {}
+client.utilsAliases = {}
+
+// Command handler
+console.log('\n------------------\nCommands');
+commandsFolder.forEach(folder => {
+  if (folder === "teste") return console.log("Achou a pasta teste");
+  var all = fs.readdirSync(`./commands/${folder}`);
+  var files = all.filter(f => {
+    let dirCheck = isDir(`./commands/${folder}/${f}`);
+    return f.split(".").slice(-1)[0] === "js" && !dirCheck;
+  });
+  var UtilsFolder = all.filter(u => {
+    let dirCheck = isDir(`./commands/${folder}/${u}`);
+    return u === "Utils" && dirCheck;
+  });
+
+  console.log(`\n${folder.replace("ZZZ", "")}/`);
+  files.forEach(f => {
+    try {
+      let pull = require(`./commands/${folder}/${f}`);
+      console.log(`- ${pull.config.name}.js: ${chalkClient.ok('OK')}`);
+      client.commands.set(pull.config.name, pull);
+      pull.config.aliases.forEach(alias => {
+        client.aliases.set(alias, pull.config.name);
+      });
+    } catch (err) {
+      /*
+      Caso aconteça algum erro
+      Cria um arquivo chamado "<nome do arquivo com erro>_Error.log"
+      */
+      console.log(`- ${f}: ${chalkClient.error('ERROR')}`);
+      console.log(`=> ${newError(err, f)}`);
+    }
+  });
+
+  // Config Utils handler
+  client.utils[folder.replace("ZZZ", "")] = {};
+  client.utilsAliases[folder.replace("ZZZ", "")] = {};
+
+  
+  UtilsFolder.forEach(u => {
+    console.log(`- ${u}/`);
+    var allUtils = fs.readdirSync(`./commands/${folder}/${u}`);
+    var UtilsFiles = allUtils.filter(uf => {
+      let dirCheck = isDir(`./commands/${folder}/${u}/${uf}`);
+      return uf.split(".").slice(-1)[0] === "js" && !dirCheck;
+    });
+
+    UtilsFiles.forEach(uf => {
+      try {
+        let pull = require(`./commands/${folder}/${u}/${uf}`);
+        console.log(`- - ${pull.config.name}.js: ${chalkClient.ok('OK')}`);
+
+        client.utils[folder.replace("ZZZ", "")][pull.config.name] = pull
+
+        pull.config.aliases.forEach(alias => {
+          client.utilsAliases[folder.replace("ZZZ", "")][alias] = pull
+        });
+      } catch (err) {
+        /*
+        Caso aconteça algum erro
+        Cria um arquivo chamado "<nome do arquivo com erro>Error.log"
+        */
+        console.log(`- - ${uf}: ${chalkClient.error('ERROR')}`);
+        console.log(`= => ${newError(err, `Utils_${uf}`)}`);
+      }
+    });
+  });
+});
+
+// Login do bot com a API do discord
+const token = process.env.TOKEN || client.config.token;
+client.login(token);
---
+++ utils.js
@@ -0,0 +1,216 @@
+// NPM requires
+const chalk = require("chalk");
+const fs = require("fs");
+const format = require("date-fns/format");
+const md5 = require("md5");
+const zlib = require("zlib");
+
+// Files requires
+const config = require("./config.json");
+
+
+//--------------------------------------------------------------------------------------------------//
+// Error handler private utils
+/**
+ * @param [fileName="null"] {String} Arquivo onde ocorreu o erro
+ * @param [IDs] {Object} IDs involvidos
+ * @param [IDs.server=0] {String|Number} ID do server
+ * @param [IDs.user=0] {String|Number} ID do usuario
+ * @param [IDs.msg=0] {String|Number} ID da mensagem
+ */
+function generateErrorID(fileName = "null", IDs = { server: 0, user: 0, msg: 0 }) {
+  let errorID = `${fileName}_${module.exports.formatDate(new Date())}_${IDs.server}-${IDs.user}-${IDs.msg}`;
+  errorID = md5(errorID);
+  return errorID;
+}
+
+//--------------------------------------------------------------------------------------------------//
+// Exports
+module.exports = {
+
+  //--------------------------------------------------------------------------------------------------//
+  //#region Chalk config
+
+  chalkClient: {
+    chalk: chalk,
+    error: chalk.bold.red,
+    warn: chalk.bold.keyword('orange'),
+    ok: chalk.bold.green
+  },
+  //#endregion
+  //--------------------------------------------------------------------------------------------------//
+  //#region Mix utils
+
+  /**
+   * Checa se o usuario do ID fornecido faz parte do time de desenvolvedores
+   * @param ID {String|Number} ID do usuario para checar
+   * @returns {Boolean}
+   */
+  isDev: (ID) => {
+    if (config.devsID.includes(ID)) return true;
+    return false;
+  },
+
+  /**
+   * Formata datas no estilo dd/MM/yyyy HH:mm:SS
+   * @param date {Date} Data para formatar
+   * @returns {String} Data formatada no estilo dd/MM/yyyy HH:mm:SS
+   */
+  formatDate: (date) => {
+    return format(date - 10800000, "dd/MM/yyyy HH:mm:ss");
+  },
+
+  /**
+   * Timer
+   * @param ms {Number} Quantidade de tempo em milisegundos
+   */
+  sleep: (ms) => {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  },
+
+  /**
+   * Retorna uma pagina
+   * @param {TextChannel} channel - o canal onde vai ser enviado 
+   * @param {Number} size - quantidade de paginas
+   * @param {Callback} Callback - a função realizada por pagina 
+   */
+  createPage: (channel, size, func) => {
+    let page = 1;
+
+    channel.send(func(1) || 'nill').then(msg => {
+      if (size == 1) return;
+      msg.react('⬅️').then(() => {
+        msg.react('➡️')
+
+        const filter = (reaction, user) => {
+          return ['➡️', '⬅️'].includes(reaction.emoji.name) && !user.bot
+        };
+
+        let collector = msg.createReactionCollector(filter, { idle: 30000 });
+
+        collector.on("collect", r => {
+          page = (page + (r.emoji.name == '➡️' ? 0 : size - 2)) % size + 1;
+
+          msg.edit(func(page) || 'nill');
+          msg.reactions.cache.each(react => {
+            react.users.cache.filter(u => !u.bot).each(u => {
+              react.users.remove(u.id);
+            });
+          });
+
+        })
+
+        collector.on("end", collected => {
+          if (collected.size > 0) return;
+          msg.reactions.removeAll();
+        });
+
+      }).catch(err => console.log(err))
+    })
+
+  },
+  //#endregion
+  //--------------------------------------------------------------------------------------------------//
+  //#region Handler utils
+
+  /**
+   * Checa se o caminho fornecido é uma pasta/diretorio
+   * @param {String} path Caminho para o diretorio a ser checado
+   * @returns {Boolean}
+   */
+  isDir: (path) => {
+    try {
+      var stat = fs.lstatSync(path);
+      return stat.isDirectory();
+    } catch (e) {
+      return false;
+    }
+  },
+
+  //#endregion
+  //--------------------------------------------------------------------------------------------------//
+  //#region Error handler utils
+
+  /**
+   * Cria o log de um novo erro
+   * @param err {Error} Erro que aconteceu
+   * @param [fileName=null] {String} Nome do arquivo onde que aconteceu o erro
+   * @param [IDs=null] {Object} IDs relacionados ao erro
+   * @param [IDs.server=null] {String|Number} ID do server
+   * @param [IDs.user=null] {String|Number} ID do usuario
+   * @param [IDs.msg=null] {String|Number} ID da mensagem
+   * @returns {String} String para logar no console
+   */
+  newError: (err, fileName = "null", IDs = { server: null, user: null, msg: null }) => {
+    if (!err) return;
+    let folder = fs.existsSync('./errors');
+    fileName = fileName.split('.')[0];
+    let errorFileName = `${fileName ? fileName + "_" : ""}${format(new Date() - 10800000, "dd:MM:yyyy_HH:mm:ss")}.json`;
+    let dados = {
+      errorID: generateErrorID(fileName, IDs),
+      msdate: Number(new Date()),
+      date: module.exports.formatDate(new Date()),
+      msg: err.message || null,
+      stack: err.stack || null,
+      IDs: IDs || null,
+      thisfile: errorFileName
+    };
+    if (!folder) {
+      fs.mkdirSync('./errors');
+    }
+    fs.writeFileSync(`./errors/${errorFileName}`, JSON.stringify(dados, null, 2), { encoding: 'utf8' });
+    return `${chalkClient.error('Erro detectado!')}\nVeja o log em: ./errors/${errorFileName}`;
+  },
+
+  /**
+   * Lista todos os erros
+   * @returns {Array} Array com os arquivos dos erros
+   */
+  listErrors: () => {
+    if (!fs.existsSync("./errors")) return [];
+    return fs.readdirSync("./errors");
+  },
+
+  /**
+   * Procura um erro usando o ID
+   * @param errorID {String|Number} ID do erro
+   * @returns {Object}
+   */
+  searchErrorByID: (errorID) => {
+    let errorFolder = module.exports.listErrors();
+    let errorSearched = errorFolder.filter(errorFile => {
+      let errorData = require(`./errors/${errorFile}`);
+      return errorData.errorID == errorID;
+    });
+    if (errorSearched.length > 0) {
+      errorSearched = errorSearched[0];
+    } else {
+      errorSearched = null;
+    }
+    return errorSearched;
+  },
+
+  /**
+   * Limpa todos os erros
+   */
+  clearAllErrors: () => {
+    let errorFolder = module.exports.listErrors();
+    errorFolder.forEach(errorFile => {
+      fs.unlink(`./errors/${errorFile}`, (err) => { if (err) console.log("=> " + newError(err, errorFile)); });
+    });
+    return;
+  },
+
+  /**
+   * Deleta um unico arquivo da pasta "errors"
+   * @param file {String} Arquivo para excluir
+   */
+  deleteError: (file) => {
+    let path = `./errors/${file}`;
+    if (!file || !fs.existsSync(path)) throw new Error('Arquivo invalido!');
+    fs.unlink(path, (err) => { if (err) console.log("\n=> " + newError(err, file)); });
+    return;
+  },
+
+  //#endregion
+}
